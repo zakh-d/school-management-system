0f2503225be242907e735996dcc1faf9a91a4500
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-04-21 13:28:11 +0200

    Basic User model creates

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+USER_ROLES = (
+    (0, "Teacher"),
+    (1, "Admin"),
+)
+
+
+class CustomUser(AbstractUser):
+
+    role = models.IntegerField(choices=USER_ROLES)
+
