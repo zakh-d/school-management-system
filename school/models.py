1a53bc6d76d7b4a79fc92619b4196e4f61a1890e
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-04-22 12:38:53 +0200

    Class Model and ClassCreateView

--- school/models.py
+++ school/models.py
@@ -4,7 +4,24 @@ from django.db import models
 
 class School(models.Model):
     id = models.UUIDField(
-         primary_key = True,
-         default = uuid.uuid4,
-         editable = False)
+         primary_key=True,
+         default=uuid.uuid4,
+         editable=False)
     name = models.CharField(max_length=255)
+
+
+class Class(models.Model):
+    id = models.UUIDField(
+         primary_key=True,
+         default=uuid.uuid4,
+         editable=False)
+    name = models.CharField(max_length=4)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField("Teacher", related_name="classes")
+
+    class Meta:
+        unique_together = ("school", "name")
+
+    def add_teachers(self, *teachers):
+        self.teachers.add(*teachers)
+        self.save()
--- school/views.py
+++ school/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from school.models import Class
 
-# Create your views here.
+
+class ClassCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+
+    model = Class
+    login_url = reverse_lazy('login')
+    fields = ('name',)
+
+    def has_permission(self):
+        """Checking whether user belongs to any school. No matter user is teacher or admin"""
+        return self.request.user.school is not None
+
+    def form_valid(self, form):
+
+        form.instance.school = self.request.user.school
+        super(ClassCreateView, self).form_valid(form)
