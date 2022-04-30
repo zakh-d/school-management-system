6c1654c3c317989cd0963a1ddd9e87892b9edfd1
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-04-30 09:43:43 +0200

    Changed CustomUser __str__ method and stylish in dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -7,14 +7,17 @@ class CustomUser(AbstractUser):
 
     class Roles(models.IntegerChoices):
 
-        TEACHER = 0, "Teacher"
-        ADMIN_MEMBER = 1, "Administration Member"
+        TEACHER = 0, 'Teacher'
+        ADMIN_MEMBER = 1, 'Administration Member'
 
     email_verified = models.BooleanField(default=False)
     role = models.IntegerField(choices=Roles.choices, default=0)
     school = models.ForeignKey(School, on_delete=models.SET_NULL,
                                null=True,
                                blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class TeacherManager(models.Manager):
