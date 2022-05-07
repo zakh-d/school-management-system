ec2a638500a6f3b56d4923126fa5263f7496c010
calidusmammal <calidusmammal@gmail.com>
2022-05-07 21:08:09 +0200

    Changes in test_school.py

--- school/tests/test_school.py
+++ school/tests/test_school.py
@@ -1,6 +1,7 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse, resolve
-
+from accounts.models import AdministrationMember
+from school.forms import CreateSchoolForm
 from school.models import School
 from school.views import CreateSchoolView
 
@@ -8,31 +9,75 @@ from school.views import CreateSchoolView
 class SchoolCreateViewTest(TestCase):
 
     def setUp(self):
-        self.client = Client()
-        self.create_url = reverse('school:create_school')
-        self.response = self.client.get(self.create_url)
+        self.school = School.objects.create(name='TEST SCHOOL')
+        admin1 = AdministrationMember.objects.create(username='testuser1')
+        admin1.set_password('testpass123')
+        admin1.save()
+        admin2 = AdministrationMember.objects.create(username='testuser2', school=self.school)
+        admin2.set_password('testpass456')
+        admin2.save()
+        self.url = reverse('school:create_school')
+
 
     def test_school_create_view_get(self):
-        view = resolve(self.create_url).func.view_class
+        self.client.login(username='testuser1', password='testpass123')
+        view = resolve(self.url).func.view_class
+
         self.assertEqual(view, CreateSchoolView)
-        self.assertEqual(self.response.status_code, 200)
 
     def test_school_create_template(self):
-        self.assertTemplateUsed(self.response, 'school/create_school.html')
+        self.client.login(username='testuser1', password='testpass123')
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed(response, 'school/create_school.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_school_create_template_forbidden_user_has_school(self):
+        self.client.login(username='testuser2', password='testpass456', school=self.school)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_school_create_template_forbidden_unregistred_user(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_school_create_form(self):
+        self.client.login(username='testuser1', password='testpass123')
+        response = self.client.post(self.url, data={'name': self.school.name})
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_school_create_form_unittest_base(self):
+        """
+        One more variant of test_school_create_form
+        """
+        self.client.login(username='testuser1', password='testpass123')
+        form = CreateSchoolForm(data={'name': self.school.name})
+
+        assert form.is_valid() is False, 'School already exit'
 
 
 class UpdateSchoolView(TestCase):
 
     def setUp(self):
-        self.client = Client()
         self.school = School.objects.create(name='TEST SCHOOL')
-        self.update_url = reverse('school:update_school', kwargs={'pk': self.school.id})
-        self.response = self.client.get(self.update_url)
+        admin = AdministrationMember.objects.create(username='testuser', school=self.school)
+        admin.set_password('testpass123')
+        admin.save()
+        self.url = reverse('school:update_school', kwargs={'pk': self.school.id})
 
     def test_school_update_view(self):
-        view = resolve(self.update_url).func.view_class
+        self.client.login(username='testuser', password='testpass123')
+        view = resolve(self.url).func.view_class
+
         self.assertEqual(view.__name__, UpdateSchoolView.__name__)
-        self.assertEqual(self.response.status_code, 200)
 
     def test_school_update_template(self):
-        self.assertTemplateUsed(self.response, 'school/create_school.html')
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed(response, 'school/update_school.html')
+        self.assertEqual(response.status_code, 200)
