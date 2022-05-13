46865b96e7b292b64fc01853d7aa879dc8c3f96d
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-13 16:05:34 +0200

    tests for validation in class creation form

--- school/tests/test_form.py
+++ school/tests/test_form.py
@@ -8,12 +8,16 @@ class TestCreateUpdateClassForm(TestCase):
         self.school = School.objects.create(name='TEST SCHOOL')
         self.test_class = Class.objects.create(name='11-A', school=self.school)
 
+    def test_name_validation(self):
+        form = CreateUpdateClassForm(data={'name': '9-A'}, school=self.school)
 
-    def test_clean_name(self):
-        form1 = CreateUpdateClassForm(data={'name': '11-A'}, school=self.school)
-        form2 = CreateUpdateClassForm(data={'name': '11-B'}, school=self.school)
-        form3 = CreateUpdateClassForm(data={'name': '11-C'}, school=None)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(len(form.errors), 0)
 
-        self.assertFalse(form1.is_valid())
-        self.assertTrue(form2.is_valid())
-        self.assertFalse(form3.is_valid())
+    def test_name_validation_not_unique_class(self):
+        form = CreateUpdateClassForm(data={'name': '11-A'}, school=self.school)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+        self.assertListEqual(form.errors['name'], ['Class 11-A already exists'])
+
